c86dca2598acf673a0e64a8253796d99c1de1e4d
abdikarimadaweh <abdik30@live.ca>
2023-05-02 00:34:29 -0400

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import header from "../img/header.png"
+    import positive from "../img/Positive.png"
+</script>
+
+
+
+<section class="headerSection">
+
+    <img class="headerImg" src={header}  alt="temp" />
+</section>
+
+<section>
+    <div class="positiveSpace">
+        <div class="positiveSpace_img">
+            <img class="positiveImg" src={positive}  alt="temp" />
+        </div>
+        <div class="positiveSpaceInfo">
+            <div class="positiveSpaceInfo_title">
+                <h1>The first step is with you</h1>
+            </div>
+            <div class="positiveSpaceInfo_details">
+                <p>Life can be challenging, especially when we experience stressors which can get in the way of us reaching our fullest potential.<br> <br>
+
+                    You can be struggling with negative thoughts, distressing emotions, going through a difficult relationship or making sense of the events in your life. You may also find yourself repeating behaviours you are unhappy with, but struggle to break the pattern.<br> <br>
+                    
+                    No matter how big or small the problem, through therapy you can attain the tools needed to help you navigate through these difficult times</p>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<style>
+    .headerSection{
+        height: 100vh;
+    }
+
+    .headerImg {
+        display: block;
+        margin: auto;
+        width: 100%;
+        height: auto;
+        max-width: 800px;
+
+    }
+    .positiveSpace {
+        display: flex;
+        flex-direction: row;
+    }
+    .positiveImg {
+        margin: auto;
+        width: 100%;
+        height: auto;
+        max-width: 600px;
+    }
+    .positiveSpaceInfo {
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        align-items: flex-start;
+        flex-direction: column;
+        margin: auto;
+    }
+    .positiveSpace_img {
+        flex: 1;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-right: 2rem ;
+    }
+    .positiveSpaceInfo_details p {
+        font-size: 20px ;
+    }
+    .positiveSpaceInfo_title h1 {
+        font-size: 43px;
+    }
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,74 @@
+<header>
+    <div class="helpBanner">
+        <p><strong><u>This is not a crisis service.</u></strong> If you are in <u>crisis</u> please call 911 or the Toronto Distress Line (416-408-4357) or<br> 
+            Canada Suicide Prevention Service (1-833-456-4566) or go to your local hospital emergency room.</p>
+    </div>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/">Starting Therapy</a>
+            </li>
+            <li>
+                <a href="/">About Me</a>
+            </li>
+            <li>
+                <a href="/">Qualifications</a>
+            </li>
+            <li>
+                <a href="/">Resources</a>
+            </li>
+            <li>
+                <a href="/">Contact</a>
+            </li>
+            <li>
+                <a href="/">Book</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+
+
+<style>
+    header {
+        display: flex;
+        flex-direction: column;
+    }
+    .helpBanner {
+        padding: 2px 0 2px 0;
+        background-color: #ffe2e2;
+        text-align: center;
+        font-size: 16px;
+
+    }
+    nav {
+        display: flex;
+        justify-content: center;
+    }
+    ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        list-style: none;
+    }
+    li {
+        display: flex;
+        margin: 0 30px 0 30px;
+        border-radius: 5px;
+        width: 120px;
+        height: 50px;
+        background-color: #5b7792;
+        text-align: center;
+        padding: 0 10px 0 10px;
+    }
+    li a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: none;
+        color: #f4f4f4;
+    }
+
+</style>
